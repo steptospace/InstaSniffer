c9145c6d7f3ac7b80952d5e736ace243a67d2782
Daniil <d.sawkow2014@gmail.com>
2021-12-04 03:23:35 +0300

    Not Stable version Вся проблема в main.go/startWork а именно в w.Update()

--- api/core.go
+++ api/core.go
@@ -2,10 +2,11 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
+	"math/rand"
 	"net/http"
+	"strconv"
 	"sync"
 )
 
@@ -31,13 +32,13 @@ type SafeMapState struct {
 }
 
 // Check all users
-func (j Worker) getAllUsers(w http.ResponseWriter, r *http.Request) {
+func (j *Worker) getAllUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(users)
 }
 
 // Info about user
-func (j Worker) GetUser(w http.ResponseWriter, r *http.Request) {
+func (j *Worker) GetUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for _, item := range users {
@@ -54,40 +55,40 @@ func (j Worker) GetUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&User{})
 }
 
-func (j Worker) GetUserStatus(w http.ResponseWriter, r *http.Request) {
+func (j *Worker) GetUserStatus(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for _, item := range users {
-		if item.Id == params["id"] {
-			err := json.NewEncoder(w).Encode(j.SafeZone.Status[item.Name])
-			if err != nil {
-				//j.GetErrStatus(item, "Error: 404")
-				http.Error(w, err.Error(), http.StatusNotFound)
-				return
-			}
-			return
-		}
+	//Check ID
+
+	err := json.NewEncoder(w).Encode(j.SafeZone.Status[params["id"]])
+	if err != nil {
+		//j.GetErrStatus(item, "Error: 404")
+		// may be this one have dummy response
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
 	}
 }
 
 // Create user
-func (j Worker) PostUser(w http.ResponseWriter, r *http.Request) {
+func (j *Worker) ParseUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var user User
-	err := json.NewDecoder(r.Body).Decode(&user)
+	id := strconv.Itoa(rand.Intn(1000000))
+	j.SafeZone.Status[id] = new(OutputData)
+	err := json.NewDecoder(r.Body).Decode(&j.SafeZone.Status[id].Output)
 	if err != nil {
-		// Check this one
-		//j.GetErrStatus(user.Name, "Error: 400")
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	users = append(users, user)
-	j.JobsChan <- user.Name
-	j.Create(user)
+	j.JobsChan <- j.SafeZone.Status[id].Output.Name
+	j.Create(id)
+	err = json.NewEncoder(w).Encode(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 // Update user
-func (j Worker) PutUser(w http.ResponseWriter, r *http.Request) {
+func (j *Worker) PutUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for index, item := range users {
@@ -104,7 +105,8 @@ func (j Worker) PutUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
-func (j Worker) DeleteUser(w http.ResponseWriter, r *http.Request) {
+// Delete user/s
+func (j *Worker) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for index, item := range users {
@@ -116,39 +118,53 @@ func (j Worker) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
-func (j Worker) Update(username string, state map[string]*OutputData, ii ImportantInfo) {
+func (j *Worker) Update(id string, ii ImportantInfo) {
 	j.SafeZone.Mu.Lock()
 	defer j.SafeZone.Mu.Unlock()
-	tt := state[username]
-	tt.State = "Done"
-	tt.Output = ii
+	j.SafeZone.Status[id] = &OutputData{
+		State:  "Done",
+		Output: ii,
+	}
 }
 
-func (j Worker) Create(user User) {
+func (j *Worker) Create(id string) {
 	j.SafeZone.Mu.Lock()
 	defer j.SafeZone.Mu.Unlock()
-	j.SafeZone.Status[user.Name] = &OutputData{
+	j.SafeZone.Status[id] = &OutputData{
 		State: "On Work",
 	}
 
 }
 
-func (j Worker) GetErrStatus(username string, state map[string]*OutputData, errCode string) {
+func (j *Worker) GetErrStatus(id string, errCode string) {
 	j.SafeZone.Mu.Lock()
 	defer j.SafeZone.Mu.Unlock()
-	j.SafeZone.Status[username] = &OutputData{
+	j.SafeZone.Status[id] = &OutputData{
 		State: errCode,
 	}
-
 }
 
-func ConnectionAPI(w Worker) {
-	fmt.Println("Server listen ...")
+func New(bufferSize int) *Worker {
+	// Буферизация на 10 элементов
+	jobs := make(chan string, bufferSize)
+	status := make(map[string]*OutputData)
+	w := &Worker{JobsChan: jobs, SafeZone: SafeMapState{Status: status}}
+	return w
+}
+
+// Не очень понятно зачем так Но если надо То ок
+// Если не сложно поясни пожалуйста
+func (j *Worker) Start() {
+	ConnectionAPI(j)
+}
+
+func ConnectionAPI(w *Worker) {
+	log.Info("Server listen ...")
 	r := mux.NewRouter()
 	r.HandleFunc("/users", w.getAllUsers).Methods("GET")
 	r.HandleFunc("/users/{id}", w.GetUser).Methods("GET")
 	r.HandleFunc("/users/{id}/status", w.GetUserStatus).Methods("GET")
-	r.HandleFunc("/users", w.PostUser).Methods("POST")
+	r.HandleFunc("/users", w.ParseUser).Methods("POST")
 	//r.HandleFunc("/users/{id}", w.PutUser).Methods("PUT")
 	//r.HandleFunc("/users/{id}", w.DeleteUser).Methods("DELETE")
 	err := http.ListenAndServe(":8000", r)
--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"InstaSniffer/api"
 	"InstaSniffer/info"
-	"fmt"
 	"github.com/kelseyhightower/envconfig"
 	log "github.com/sirupsen/logrus"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 	"time"
 )
@@ -23,27 +21,25 @@ var dummy = api.ImportantInfo{
 }
 
 type envConfig struct {
-	Threads int `required:"true" envconfig:"THR"`
+	Threads    int `required:"true" envconfig:"THR"`
+	BufferSize int `required:"true" envconfig:"BS"`
 }
 
-func startWork(id int, w api.Worker) {
+func startWork(id int, w *api.Worker) {
 	for j := range w.JobsChan {
-		fmt.Println("Worker", id, "starting new:", j)
+		log.Info("Worker: ", id, " starting new: ", j)
 		err, ii := info.UploadData(j)
 		if err != nil {
-			w.Update(j, w.SafeZone.Status, dummy)
+			w.Update(j, dummy)
 			log.Error(err)
 		}
 		time.Sleep(time.Millisecond)
-		fmt.Println("Worker", id, "finished:", j)
-		w.Update(j, w.SafeZone.Status, ii)
+		log.Info("Worker", id, "finished:", j)
+		w.Update(j, ii)
 	}
 }
 
 func coreWorker() {
-	// Буферизация на 10 элементов
-	jobs := make(chan string, 10)
-
 	//env magic
 	var env envConfig
 	err := envconfig.Process("THR", &env)
@@ -52,15 +48,14 @@ func coreWorker() {
 	}
 
 	//request status
-	status := make(map[string]*api.OutputData)
-	var mu sync.Mutex
-	w := api.Worker{JobsChan: jobs, SafeZone: api.SafeMapState{Status: status, Mu: mu}}
 
-	go api.ConnectionAPI(w)
+	w := api.New(env.BufferSize)
+	go w.Start()
 
 	for i := 1; i <= env.Threads; i++ {
 		go startWork(i, w)
 	}
+
 }
 
 //go:generate oapi-codegen -generate types -package api -o api/api.gen.go swagger.yaml
@@ -76,17 +71,19 @@ func main() {
 	os.Exit(1)
 
 	// TODO:
-	// + 1. Заменить в свагере структуру респонза для /users/{id}/status
-	// + 2. dummy можно вынести в var (снаружи функций), чтобы не передавать через параметры
-	//3. main.go 39, 58 - при вызове функций передаём туда w (api.Worker) - и при этом происходит копирование объекта мьютекса (mu).
-	//Это некорректно. Везде при вызове функций следует передавать указатель на api.Worker. Красиво было бы сделать так:
-	// + 4. Получение переменных окружения желательно вынести в отдельную функцию, в идеале создать структуру (типа envConfig).
-	//		И туда сохранить нужные переменные окружения (на случай, если их больше одной). В этой же функции весь парсинг переменных.
-	//		Переменную count лучше назвать countWorkers (чтобы было сразу понятно о чём это)
-	// + 5. Функцию worker лучше назвать startWork или как то глаголом (это по неймингу функций - должно быть действие)
-	// + 6. parser.go - 56, 58 - не надо 2 раза вызывать infoAboutUser - лучше сохранить в переменную
-	// + 7. parser.go - 72 - лучше сделать так:
-	// + 8. Убрать ненужные роуты из core.go
-	// + 9. Unlock() в мьютексах можно через defer делать
+	// + 3. main.go 39, 58 - при вызове функций передаём туда w (api.Worker) - и при этом происходит копирование объекта мьютекса (mu).
+	//		Это некорректно. Везде при вызове функций следует передавать указатель на api.Worker. Красиво было бы сделать так:
+	// + 1. Поменять название метода PostUser на что-то более осмысленное (напр. ParseUser)
+	//2. Если не может спарсить данные должен быть ответ dummy, но в ответе - пустая структура (надо проверить, почему так)
+	// + 3. Сделать то что я писала по 3 пункту (либо пиши, что именно не получается)
+	// + 4. Заменить все принты на логи (log.info..)
+	// +5. В пост запросе возвращать id (то есть генерить id на сервере, желательно uid).
+	//		Соотвественно в теле запроса останется только name. Так это будет ближе к реальности.
+	// + 6. Update, GetErrStatus - убрать из параметров state, т.к. мапа уже есть внутри j (Worker)
+	// + 7. В Update сделать обновление, по аналогии с getErrStatus
+	//8. Убрать глобальную переменную var users []User. Для выдачи статуса использовать мапу, где храним всю инфу о юзерах
+	//9. Если ввожу несуществующий айди при проверке статуса - выдаётся статус 200 - всё ок (должна выводиться ошибка + статус не 200).
+	//Плюс описание ошибки планировали выводить внутри OutputData, это уже где-то есть?
+	//10* Не запускать задачу, если она уже в работе
 	// * БД - сохранять результаты в таблицу users
 }
